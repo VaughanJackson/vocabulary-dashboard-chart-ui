fff5695ff4546797a1e1ad7bb8708fac390f0687
VaughanJackson <vaughanjackson2001@yahoo.com>
2018-12-25 20:49:15 +0000

    Vocabulary Chart: Wrap the chart within an ES6 module.

--- VocabularyChart.js
+++ VocabularyChart.js
@@ -1,4 +1,4 @@
-class vocabularyChart {
+export default class {
 
     constructor() {
 
@@ -261,15 +261,3 @@ class vocabularyChart {
     }
 
 }
-
-
-const chart = new vocabularyChart().margin({top: 300, right: 20, bottom: 200, left: 50})
-    .height(800)
-    .width(1600);
-
-// Get the data
-d3.json("http://localhost:8080/vocabulary").then((characters) => {
-    return chart.draw(d3.select("body"), characters);
-}).catch((error) => {
-   return console.log(error);
-});
